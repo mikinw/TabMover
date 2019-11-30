7d833fd94d04fe9d075463e630f3f797afcef69e
Dmitry Murzin <diraria@yandex.ru>
2019-12-01 02:58:59 +0300

    Use gradle as build system

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.4.14"
+    java
+    kotlin("jvm") version "1.3.61"
+}
+
+group = "com.mnw.tabmover"
+version = "1.3.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2019.3"
+}
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    sinceBuild("100")
+    untilBuild("700")
+}

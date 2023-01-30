66550496844145a90d99dbbccc3b44a01959153b
Miklos Jakab <jakab.miklos@gmail.com>
2023-01-30 11:34:34 +0100

    version++ + gradle update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-    id("org.jetbrains.intellij") version "0.4.14"
+    id("org.jetbrains.intellij") version "1.12.0"
     java
     kotlin("jvm") version "1.3.61"
 }
 
 group = "com.mnw.tabmover"
-version = "1.4.1"
+version = "1.5.0"
 
 repositories {
     mavenCentral()
@@ -13,13 +13,14 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testCompile("junit", "junit", "4.12")
+//    testCompile("junit", "junit", "4.12")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version = "2019.3"
-    updateSinceUntilBuild = false
+    version.set("2022.1.1")
+    updateSinceUntilBuild.set(false)
+    plugins.set(listOf("com.intellij.java"))
 }
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -30,6 +31,12 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    patchPluginXml {
+        version.set("${project.version}")
+        sinceBuild.set("221")
+        untilBuild.set("223.*")
     }
 }
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
